d539ef3737425cc846498e364016fb5d2d9d6112
John Doe <kakdechaitrali16@gmail.com>
2025-01-31 22:09:29 +0530

    error resolved

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/auth', authRoutes);
 app.use('/api', gadgetRoutes);
 app.get("/", (req, res) => {
 
-    res.send("Hello world");
+    res.send("Hello worl");
   });
 
 app.listen(PORT, () => {
