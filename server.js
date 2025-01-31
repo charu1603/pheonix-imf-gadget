f4e90d92b377bd7776d0dd5f5e097f969af572f8
John Doe <kakdechaitrali16@gmail.com>
2025-01-31 20:33:39 +0530

    all api's done

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/auth', authRoutes);
 app.use('/api', gadgetRoutes);
 app.get("/", (req, res) => {
 
-    res.send("Hello World");
+    res.send("Hello");
   });
 
 app.listen(PORT, () => {
